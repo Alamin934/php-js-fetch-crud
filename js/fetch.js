0edb3bb063f559766f72da87151059b608bde59b
Alamin934 <islamalamin934@gmail.com>
2023-11-09 08:51:47 +0600

    creating a javascript fetch method php crud project

---
+++ js/fetch.js
@@ -0,0 +1,24 @@
+// function for load student record in table on page load
+
+
+// Open Add new student Modal Box 
+
+
+// Hide Modal Box / Popup Box
+
+// Add Student Record
+
+
+// Open Update Modal Box and show Student record in it.
+
+
+// Update student record
+
+
+// Delete student record
+
+
+// Search student record
+
+
+//show error / success message
